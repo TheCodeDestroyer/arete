c45932626b60b87a1bf3d33d860aeb16fba6fcd9
Nace Logar <the.code.destroyer@gmail.com>
2015-02-15 19:03:53 +0100

    Implementing workout form

--- www/js/workout/controllers/wrkDetailCtrl.js
+++ www/js/workout/controllers/wrkDetailCtrl.js
@@ -1,8 +1,9 @@
-angular.module('arete.controllers').controller('wrkDetailCtrl', function ($scope, $translate, $stateParams) {
+angular.module('arete.controllers').controller('wrkDetailCtrl', function ($scope, $translate, $stateParams, Workout) {
     'use strict';
 
     var vm = this;
     vm.workoutId = $stateParams.id;
+    vm.workout = {};
     vm.selected = '';
     vm.days = [];
     vm.repeatList = [
@@ -10,21 +11,66 @@ angular.module('arete.controllers').controller('wrkDetailCtrl', function ($scope
         { id: 2, label: 'general.repeat.WEEKLY' }
         //{ id: 3, label: 'general.repeat.MONTHLY' } TODO: Not supported for now
     ];
-    vm.selectedRepeatType = _.first(vm.repeatList).id;
+    vm.workout.selectedRepeatType = _.first(vm.repeatList).id;
+
+
 
     for (var i = 1; i < 8; i++) {
         var day = { id: i, label: 'general.days.' + i };
         vm.days.push(day);
     }
 
+    Workout.load(vm.workoutId, function(workout) {
+        vm.workout = workout;
+        if(!$scope.$$phase) {
+            $scope.$apply();
+        }
+    });
+
     vm.showWeeklyLabel = function() {
-        var parsedSelectedRepeatType = parseInt(vm.selectedRepeatType),
+        var parsedSelectedRepeatType = parseInt(vm.workout.selectedRepeatType),
             weeklyRepeatListId = _.findWhere(vm.repeatList, { label: 'general.repeat.WEEKLY' }).id;
         return parsedSelectedRepeatType === weeklyRepeatListId;
     };
 
-    vm.saveWorkout = function () {
-        console.log('gfsdgf');
+    vm.saveWorkout = function (workoutModel) {
+        var workout,
+            repeatDaysArray = [];
+
+        if (vm.workout.id) {
+            workout = new Workout();
+        }
+        else {
+            workout = vm.workout;
+        }
+
+        if (workout.title !== workoutModel.title) {
+            workout.title = workoutModel.title;
+        }
+
+        if (workout.time !== workoutModel.time) {
+            workout.time = workoutModel.time;
+        }
+
+        if (workout.repeat !== workoutModel.repeat) {
+            workout.repeat = workoutModel.repeat;
+        }
+
+        if (workout.repeatType !== workoutModel.selectedRepeatType) {
+            workout.repeatType = workoutModel.selectedRepeatType;
+        }
+
+        for(var propertyName in workoutModel.daysInWeek) {
+            if (workoutModel.daysInWeek.hasOwnProperty(propertyName)) {
+                var indexDayInWeek = parseInt(propertyName);
+                repeatDaysArray.push(indexDayInWeek);
+            }
+        }
+
+        if (repeatDaysArray && !_.includes(workout.repeatedDays, repeatDaysArray)) {
+            workout.repeatedDays = repeatDaysArray;
+        }
+
     };
 
 });
