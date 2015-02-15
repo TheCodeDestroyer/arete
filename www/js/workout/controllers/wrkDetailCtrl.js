8353ab9883340fdc84c93bcb8cee75e0969d92f8
Nace Logar <the.code.destroyer@gmail.com>
2015-02-15 18:17:02 +0100

    Implementing workout form

--- www/js/workout/controllers/wrkDetailCtrl.js
+++ www/js/workout/controllers/wrkDetailCtrl.js
@@ -5,10 +5,26 @@ angular.module('arete.controllers').controller('wrkDetailCtrl', function ($scope
     vm.workoutId = $stateParams.id;
     vm.selected = '';
     vm.days = [];
+    vm.repeatList = [
+        { id: 1, label: 'general.repeat.DAILY' },
+        { id: 2, label: 'general.repeat.WEEKLY' }
+        //{ id: 3, label: 'general.repeat.MONTHLY' } TODO: Not supported for now
+    ];
+    vm.selectedRepeatType = _.first(vm.repeatList).id;
 
     for (var i = 1; i < 8; i++) {
         var day = { id: i, label: 'general.days.' + i };
         vm.days.push(day);
     }
+
+    vm.showWeeklyLabel = function() {
+        var parsedSelectedRepeatType = parseInt(vm.selectedRepeatType),
+            weeklyRepeatListId = _.findWhere(vm.repeatList, { label: 'general.repeat.WEEKLY' }).id;
+        return parsedSelectedRepeatType === weeklyRepeatListId;
+    };
+
+    vm.saveWorkout = function () {
+        console.log('gfsdgf');
+    };
 
 });
